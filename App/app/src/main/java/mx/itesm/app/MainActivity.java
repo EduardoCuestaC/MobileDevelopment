f0ba4206dcaf04ac855b8325b7a3dbace2ea1505
EduardoCuestaC <A00570171@itesm.mx>
2019-02-18 18:14:38 -0600

    lab3

--- App/app/src/main/java/mx/itesm/app/MainActivity.java
+++ App/app/src/main/java/mx/itesm/app/MainActivity.java
@@ -1,16 +1,27 @@
 package mx.itesm.app;
 
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.annotation.Target;
 
 public class MainActivity extends AppCompatActivity {
     Button bTargetAct;
     EditText etMain;
     String sent;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
         bTargetAct = findViewById(R.id.b1);
         etMain = findViewById(R.id.et1);
+
+        context = getApplicationContext();
 
         bTargetAct.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +42,77 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Toast mToast = new Toast(context);
+        mToast.makeText(context, "A toast", Toast.LENGTH_LONG);
+
+        AlertDialog.Builder mDialogBase = new AlertDialog.Builder(this);
+        mDialogBase.setTitle("Confirmation dialog");
+        mDialogBase.setMessage("Dialog text");
+        mDialogBase.setIcon(R.mipmap.ic_launcher);
+        mDialogBase.setNeutralButton("Close",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(context, "Dialog closed", Toast.LENGTH_LONG).show();
+                    }
+                });
+        mDialogBase.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(context, "Action approved", Toast.LENGTH_LONG).show();
+                    }
+                });
+        mDialogBase.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(context, "Action canceled", Toast.LENGTH_LONG).show();
+                    }
+                });
+        AlertDialog mAlertDialog = mDialogBase.create();
+        mAlertDialog.show();
+
+        AlertDialog.Builder textDialog = new AlertDialog.Builder(this);
+        textDialog.setTitle("Text dialog");
+        textDialog.setMessage("Text dialog text");
+        textDialog.setIcon(R.mipmap.ic_launcher);
+
+        final EditText etDialog = new EditText(context);
+        etDialog.setTextColor(Color.BLUE);
+        textDialog.setView(etDialog);
+
+        textDialog.setNeutralButton("Close",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Dialog closed", Toast.LENGTH_LONG).show();
+                    }
+                });
+        textDialog.setPositiveButton("Submit",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String s = etDialog.getEditableText().toString();
+                        Toast.makeText(context, s, Toast.LENGTH_LONG);
+                    }
+                });
+        AlertDialog mTextAlertDialog = textDialog.create();
+        mTextAlertDialog.show();
+
+        int M_NOTIF_ID = 1111;
+        Notification.Builder mNotifBuilder = new Notification.Builder(this);
+        mNotifBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mNotifBuilder.setContentTitle("Content title");
+        mNotifBuilder.setContentText("Content text");
+
+        Intent intentMNotif = new Intent(context, TargetActivity.class);
+        intentMNotif.putExtra("ValueToSend", "Came from notification");
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intentMNotif, 0);
+
+        mNotifBuilder.setContentIntent(contentIntent);
+        NotificationManager notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notifManager.notify(M_NOTIF_ID, mNotifBuilder.build());
     }
 }
